27ece2ee31511ad466a1f0db0d4ec84ac6eb6324
Audrey Zebaze <audrey.sobgouzebaze@gmail.com>
2021-11-08 23:09:40 +0100

    Show image picker for image selection

--- PhotoRoomAPIExample/Network/RemoveBackgroundService.swift
+++ PhotoRoomAPIExample/Network/RemoveBackgroundService.swift
@@ -7,17 +7,45 @@
 
 import Foundation
 import PhotoRoomKit
+import SwiftUI
+import Combine
+
+enum RemoveBackgroundServiceError: Error {
+    case segmentationFailed
+    case noImageReturned
+}
 
 protocol RemoveBackgroundGateway {
-    
+    func removeBackground(_ originalImage: UIImage) -> AnyPublisher<UIImage, RemoveBackgroundServiceError>
 }
 
 final class RemoveBackgroundService: RemoveBackgroundGateway {
     
-    private let segmentationService: SegmentationService = SegmentationService(apiKey: Constants.API_KEY)
+    private let segmentationService: SegmentationService
     
-    init() {
-        
+    init(segmentationService: SegmentationService = SegmentationService(apiKey: Constants.API_KEY)) {
+        self.segmentationService = segmentationService
+    }
+    
+    func removeBackground(_ originalImage: UIImage) -> AnyPublisher<UIImage, RemoveBackgroundServiceError> {
+        Deferred {
+            Future<UIImage, RemoveBackgroundServiceError> { promise in
+                self.segmentationService.segment(image: originalImage) { (image, error) in
+                    DispatchQueue.main.async {
+                        if error != nil {
+                            promise(.failure(.segmentationFailed))
+                        }
+                        guard let image = image else {
+                            // No image returned
+                            promise(.failure(.noImageReturned))
+                            return
+                        }
+                        // All good
+                        promise(.success(image))
+                    }
+                }
+            }
+        }.eraseToAnyPublisher()
     }
     
 }
