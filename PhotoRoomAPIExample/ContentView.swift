578536a791590b6efd5caf848310563739a3ac19
Audrey Zebaze <audrey.sobgouzebaze@gmail.com>
2021-11-08 19:56:12 +0100

    Initial Commit: project initialization

---
+++ PhotoRoomAPIExample/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  PhotoRoomAPIExample
+//
+//  Created by Audrey SOBGOU ZEBAZE on 08/11/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ PhotoRoomAPIExample/PhotoRoomAPIExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  PhotoRoomAPIExampleApp.swift
+//  PhotoRoomAPIExample
+//
+//  Created by Audrey SOBGOU ZEBAZE on 08/11/2021.
+//
+
+import SwiftUI
+
+@main
+struct PhotoRoomAPIExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
