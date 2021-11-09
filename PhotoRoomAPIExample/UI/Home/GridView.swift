fae42f6dfebbd8eee2ccd9a30ca6f4cfd4a95ac4
Audrey Zebaze <audrey.sobgouzebaze@gmail.com>
2021-11-09 13:30:44 +0100

    Add PreviewView and HomeView

---
+++ PhotoRoomAPIExample/UI/Home/GridView.swift
@@ -0,0 +1,20 @@
+//
+//  GridView.swift
+//  PhotoRoomAPIExample
+//
+//  Created by Audrey SOBGOU ZEBAZE on 09/11/2021.
+//
+
+import SwiftUI
+
+struct GridView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct GridView_Previews: PreviewProvider {
+    static var previews: some View {
+        GridView()
+    }
+}
---
+++ PhotoRoomAPIExample/UI/Home/PreviewView.swift
@@ -0,0 +1,77 @@
+//
+//  PreviewView.swift
+//  PhotoRoomAPIExample
+//
+//  Created by Audrey SOBGOU ZEBAZE on 09/11/2021.
+//
+
+import SwiftUI
+
+struct PreviewView: View {
+    
+    @Binding var imageState: ImageState
+    
+    @Binding var text: String
+    
+    var action: () -> Void = {}
+    
+    var blurAmount: CGFloat = 1
+    
+    @Binding var image: UIImage?
+    
+    public init(image: Binding<UIImage?>,
+                imageState: Binding<ImageState>,
+                text: Binding<String>,
+                action: @escaping () -> Void) {
+        self._image = image
+        self._imageState = imageState
+        self._text = text
+        self.action = action
+    }
+    
+    var body: some View {
+        
+        VStack {
+            if let image = image {
+                ZStack {
+                    Image(uiImage: image)
+                        .resizable()
+                        .scaledToFill()
+                        .frame(minWidth: 0, maxWidth: .infinity)
+                        .edgesIgnoringSafeArea(.all)
+                    if imageState == .processing {
+                        Text("Processing")
+                    }
+                }
+                
+            } else {
+                Spacer()
+            }
+           
+            Button(action: action) {
+                HStack {
+                    Image(systemName: "photo")
+                        .font(.system(size: 20))
+                    
+                    Text(text)
+                        .font(.headline)
+                }
+                .frame(minWidth: 0, maxWidth: .infinity, minHeight: 0, maxHeight: 50)
+                .background(Color.blue)
+                .foregroundColor(.white)
+                .cornerRadius(20)
+                .padding(.horizontal)
+            }
+        }.padding()
+    }
+}
+
+struct PreviewView_Previews: PreviewProvider {
+    static var previews: some View {
+        PreviewView(image: .constant(UIImage(systemName: "photo")!),
+                    imageState: .constant(.processing),
+                    text: .constant("Processing background")) {
+            print("Did something")
+        }
+    }
+}
